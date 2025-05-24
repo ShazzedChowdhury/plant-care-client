930e83bda552aa64aa3a3acde95a31a457d1be2a
shazzed-76 <shazzed7656@gmail.com>
2025-05-24 13:01:03 +0600

    added hover effect in user profile using react-tooltip

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Tooltip } from 'react-tooltip';
 
 const UserProfile = ({user}) => {
     return (
       <div className="avatar">
         <div className="ring-primary ring-offset-base-100 w-10 rounded-full ring-2 ring-offset-2">
-          <img src={user?.photoURL} />
+          {/* user image */}
+          <img data-tooltip-id='user-profile' className='w-full h-full object-cover' src={user?.photoURL} />
+
+          <Tooltip
+            id='user-profile'
+            place="bottom-start"
+            className="z-50"
+            delayHide={100}
+          >
+            <h3 className='text-sm font-normal'>{user?.displayName}</h3>
+          </Tooltip>
         </div>
       </div>
     );
